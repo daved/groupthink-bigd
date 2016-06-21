922f7f80cb8d35d707b9d7fd647bdeb0f6f887a5
Daved <daved@codemodus.com>
2016-06-21 16:34:22 -0700

    Add comments.

--- main.go
+++ main.go
@@ -16,20 +16,26 @@ import (
 )
 
 const (
+	// width controls the amount of goroutines running the digest function.
 	width = 16
 )
 
+// filesInfo holds a slice of os.FileInfo along with the directory the
+// contents came from.
 type filesInfo struct {
 	dir string
 	fsi []os.FileInfo
 }
 
+// result holds a full file path, processed data, and error (if any).
 type result struct {
 	path string
 	data string
 	err  error
 }
 
+// gzFilesInfoIn is a convenience function which grabs all gzipped files
+// within the provided directory with a depth of 1.
 func gzFilesInfoIn(dir string) (*filesInfo, error) {
 	fsi, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -37,6 +43,7 @@ func gzFilesInfoIn(dir string) (*filesInfo, error) {
 	}
 
 	for k := len(fsi) - 1; k >= 0; k-- {
+		// Remove directories, and files without the correct extension.
 		if fsi[k].IsDir() || !strings.HasSuffix(fsi[k].Name(), ".gz") {
 			fsi = append(fsi[:k], fsi[k+1:]...)
 		}
@@ -45,6 +52,9 @@ func gzFilesInfoIn(dir string) (*filesInfo, error) {
 	return &filesInfo{dir: dir, fsi: fsi}, nil
 }
 
+// digest processes the file located at the currently provided path, and
+// sends out a new result. It could be used, instead, to communicate with
+// relevant micorservices.
 func digest(done <-chan struct{}, paths <-chan string, c chan<- result) {
 	for p := range paths {
 		r := result{path: p}
@@ -85,6 +95,8 @@ func digest(done <-chan struct{}, paths <-chan string, c chan<- result) {
 	}
 }
 
+// funnel receives a filesInfo type, spawns goroutines (determined by the
+// const "width"), and closes
 func funnel(done <-chan struct{}, fsi *filesInfo) (<-chan result, <-chan error) {
 	c := make(chan result)
 	errc := make(chan error, 1)
@@ -93,6 +105,7 @@ func funnel(done <-chan struct{}, fsi *filesInfo) (<-chan result, <-chan error) 
 	paths := make(chan string)
 
 	go func() {
+		// anon go func sends paths down the correct channel.
 		go func() {
 			for _, v := range fsi.fsi {
 				select {
@@ -105,6 +118,7 @@ func funnel(done <-chan struct{}, fsi *filesInfo) (<-chan result, <-chan error) 
 			close(paths)
 		}()
 
+		// setup digesters by width.
 		wg.Add(width)
 		for i := 0; i < width; i++ {
 			go func() {
@@ -113,6 +127,7 @@ func funnel(done <-chan struct{}, fsi *filesInfo) (<-chan result, <-chan error) 
 			}()
 		}
 
+		// wait and close result channel after paths have been processed.
 		go func() {
 			wg.Wait()
 			close(c)
@@ -123,29 +138,36 @@ func funnel(done <-chan struct{}, fsi *filesInfo) (<-chan result, <-chan error) 
 }
 
 func main() {
+	// Ignore system signals.
 	sm := sigmon.New(nil)
 	sm.Run()
 
+	// Define and parse app flags.
 	profM := flag.String("profmem", "",
 		`Run memory profile and write to named file.`)
 
 	flag.Parse()
 
+	// Get populated filesInfo type.
 	fsi, err := gzFilesInfoIn("./testdata/")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
+	// Setup done channel for cancellation.
 	done := make(chan struct{})
 	defer close(done)
 
+	// Close done channel on any system signal.
 	sm.Set(func(sm *sigmon.SignalMonitor) {
 		close(done)
 	})
 
+	// Get results and error channels (is non-blocking).
 	rs, errc := funnel(done, fsi)
 
+	// Print result contents or error.
 	for r := range rs {
 		select {
 		case err := <-errc:
@@ -156,6 +178,7 @@ func main() {
 		}
 	}
 
+	// Write heap profile if flag is set.
 	if err := vitals.WriteHeapProfile(*profM); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
